675ee83aba81ec1e387a9ae88316ae417dde20dc
FawazofLagos01 <aybest1245@gmail.com>
2025-10-19 17:40:12 +0100

    Fix task_4.sql to use DATABASE() for checker compatibility

--- task_4.sql
+++ task_4.sql
@@ -1,11 +1,14 @@
--- Show full description of the 'books' table without using DESCRIBE or EXPLAIN
-SELECT 
+-- Script to display full description of the 'books' table without using DESCRIBE or EXPLAIN
+
+SELECT
     COLUMN_NAME,
     COLUMN_TYPE,
     IS_NULLABLE,
     COLUMN_KEY,
     COLUMN_DEFAULT,
     EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-AND TABLE_NAME = 'books';
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'books';
