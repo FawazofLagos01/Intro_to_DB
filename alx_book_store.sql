ef7ada68f56adb45c47679b3fdba7bc2b0a29096
FawazofLagos <aybest12452gmail.com>
2025-10-24 01:22:43 -0500

    Add alx_book_store.sql database schema

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,41 +1,53 @@
--- alx_book_store.sql
-
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE Authors (
-    author_id INT PRIMARY KEY AUTO_INCREMENT,
-    author_name VARCHAR(215)
+CREATE TABLE IF NOT EXISTS AUTHORS (
+	author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
 );
 
-CREATE TABLE Books (
-    book_id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(130),
-    author_id INT,
-    price DOUBLE,
+CREATE TABLE IF NOT EXISTS BOOKS (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+    CONSTRAINT fk_books_author
+        FOREIGN KEY (author_id)
+        REFERENCES AUTHORS(author_id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 );
 
-CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_name VARCHAR(215),
-    email VARCHAR(215),
+CREATE TABLE IF NOT EXISTS CUSTOMERS (
+	customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL UNIQUE,
     address TEXT
 );
 
-CREATE TABLE Orders (
-    order_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT,
-    order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+CREATE TABLE IF NOT EXISTS ORDERS (
+	order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    CONSTRAINT fk_order_customer
+		FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
-CREATE TABLE Order_Details (
-    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+CREATE TABLE IF NOT EXISTS ORDER_DETAILS (
+	orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
+    CONSTRAINT fk_orderdetails_order
+      FOREIGN KEY (order_id) REFERENCES ORDERS(order_id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+    CONSTRAINT fk_orderdetails_book
+      FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE
 );
+        
